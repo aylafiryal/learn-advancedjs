988a8f6a00d4ef2ee73a421e8f295a60a46e1dc9
Alya <auliafiryalsyarifa@gmail.com>
2024-01-12 21:26:56 +0700

    Create object with Object.create()

--- script.js
+++ script.js
@@ -29,16 +29,41 @@
 // let EricC = King("Cartman", 20);
 
 // 3. Constructor function (new)
-function King(name, energy) {
-  this.name = name;
-  this.energy = energy;
+// function King(name, energy) {
+//   this.name = name;
+//   this.energy = energy;
 
-  this.power = function (cheesepoof) {
+//   this.power = function (cheesepoof) {
+//     this.energy = this.energy + cheesepoof;
+//     console.log(`Hi, your majesty, King ${this.name}.
+//         Your energy now at ${this.energy}`);
+//   };
+// }
+
+// let KyleB = new King("Kyle", 10);
+// let EricC = new King("Cartman", 20);
+
+// 4. Object.create()
+const kingMethods = {
+  power: function (cheesepoof) {
     this.energy = this.energy + cheesepoof;
     console.log(`Hi, your majesty, King ${this.name}.
-        Your energy now at ${this.energy}`);
-  };
+            Your energy now at ${this.energy}`);
+  },
+  attack: function (damage) {
+    this.energy = this.energy - damage;
+    console.log(`Goodluck, King ${this.name}.
+            Your energy now at ${this.energy}`);
+  },
+};
+
+function King(name, energy) {
+  let king = Object.create(kingMethods);
+  king.name = name;
+  king.energy = energy;
+
+  return king;
 }
 
-let KyleB = new King("Kyle", 10);
-let EricC = new King("Cartman", 20);
+let KyleB = King("Kyle", 10);
+let EricC = King("Cartman", 20);
