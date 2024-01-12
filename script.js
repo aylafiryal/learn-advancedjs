600bbc7a92928089230f66101d1ecd6084cf29d1
Alya <auliafiryalsyarifa@gmail.com>
2024-01-12 21:50:57 +0700

    Create object using Prototype

--- script.js
+++ script.js
@@ -44,26 +44,41 @@
 // let EricC = new King("Cartman", 20);
 
 // 4. Object.create()
-const kingMethods = {
-  power: function (cheesepoof) {
-    this.energy = this.energy + cheesepoof;
-    console.log(`Hi, your majesty, King ${this.name}.
-            Your energy now at ${this.energy}`);
-  },
-  attack: function (damage) {
-    this.energy = this.energy - damage;
-    console.log(`Goodluck, King ${this.name}.
-            Your energy now at ${this.energy}`);
-  },
-};
+// const kingMethods = {
+//   power: function (cheesepoof) {
+//     this.energy = this.energy + cheesepoof;
+//     console.log(`Hi, your majesty, King ${this.name}.
+//             Your energy now at ${this.energy}`);
+//   },
+//   attack: function (damage) {
+//     this.energy = this.energy - damage;
+//     console.log(`Goodluck, King ${this.name}.
+//             Your energy now at ${this.energy}`);
+//   },
+// };
 
+// function King(name, energy) {
+//   let king = Object.create(kingMethods);
+//   king.name = name;
+//   king.energy = energy;
+
+//   return king;
+// }
+
+// let KyleB = King("Kyle", 10);
+// let EricC = King("Cartman", 20);
+
+// 5. Prototype
 function King(name, energy) {
-  let king = Object.create(kingMethods);
-  king.name = name;
-  king.energy = energy;
-
-  return king;
+  this.name = name;
+  this.energy = energy;
 }
 
-let KyleB = King("Kyle", 10);
-let EricC = King("Cartman", 20);
+King.prototype.power = function (cheesepoof) {
+  this.energy = this.energy + cheesepoof;
+  console.log(`Hi, your majesty, King ${this.name}.
+        Your energy now at ${this.energy}`);
+};
+
+let KyleB = new King("Kyle", 10);
+let EricC = new King("Cartman", 20);
