ecd36e50ccc853dfce694150909516251d791c5c
Alya <auliafiryalsyarifa@gmail.com>
2024-01-13 21:09:54 +0700

    Closure

--- closure.js
+++ closure.js
@@ -1,17 +1,34 @@
+// function init() {
+//   let nama = "Kyle";
+//   let umur = 33;
+//   function tampilNama() {
+//     // closure, lexical
+//     console.log(nama);
+//   }
+//   function tampilNama2() {
+//     // NOT closure
+//     let nama = "Craig";
+//     console.log(nama);
+//   }
+//   //   tampilNama();
+//   console.dir(tampilNama);
+// }
+
+// init();
+
+// function init() {
+//   function tampilNama(nama) {
+//     console.log(nama);
+//   }
+//   return tampilNama;
+// }
+
 function init() {
-  let nama = "Kyle";
-  let umur = 33;
-  function tampilNama() {
-    // closure, lexical
+  return function (nama) {
     console.log(nama);
-  }
-  function tampilNama2() {
-    // NOT closure
-    let nama = "Craig";
-    console.log(nama);
-  }
-  //   tampilNama();
-  //   console.dir(tampilNama);
+  };
 }
 
-init();
+let panggilNama = init();
+panggilNama("Kyle");
+panggilNama("Broflovski");
