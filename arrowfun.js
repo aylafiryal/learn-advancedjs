f13692e7bdb1a46c50859fc377a994258e65a515
Alya <auliafiryalsyarifa@gmail.com>
2024-01-14 13:36:31 +0700

    (Masih) Arrow Function

---
+++ arrowfun.js
@@ -0,0 +1,18 @@
+// Function Expression
+// const tampilNama = function (nama) {
+//     return `Halo, ${nama}`;
+// }
+// console.log(tampilNama('Kyle'));
+
+// Arrow function with 1 parameter
+// With implisit return
+// const tampilNama = (nama) => `Halo, ${nama}`;
+// console.log(tampilNama("Kyle Broflovski"));
+
+// 2 parameter
+// const tampilNama = (nama, heroName) => `Halo, ${nama}. Kamu adalah ${heroName}`;
+// console.log(tampilNama("Kyle Broflovski", "Human Kite"));
+
+// Tanpa parameter
+// const tampilNama = () => `Halo, Kyle Broflovski`;
+// console.log(tampilNama());
